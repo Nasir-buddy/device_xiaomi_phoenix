76e66459f2f2557e0246a8124997136260283739
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-04-24 09:42:14 +0530

    Revert "phoenix: let's build los"
    This reverts commit fd1aa4b09b095f1a24251a1a90b863621556ce16.

--- arrow_phoenix.mk
+++ arrow_phoenix.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_phoenix
+PRODUCT_NAME := arrow_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
